5d21e1822911d8fe5988e7a284ae4af314516d99
JeongJu Kim <haje01@gmail.com>
2017-06-30 08:27:21 +0000

    windows verpatch

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-pyinstaller swak/win_svc.py --hidden-import=win32timezone --onefile --name swak --icon swak.ico --version-file version.py
-rem run service test only
+set VERSION=0.1.0.0
+pyinstaller swak\win_svc.py --hidden-import=win32timezone --onefile --name swak --icon swak.ico
+tools\verpatch.exe dist\swak.exe %VERSION% /va /pv %VERSION% /s description "Swak: Multi-Agent Service" /s product "Swak" /s copyright "Copyright 2017, Swak contirubutors"
 set SWAK_BUILD=TRUE
 coverage run --source swak --parallel-mode --module pytest tests -k test_svc
