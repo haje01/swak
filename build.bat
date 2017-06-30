ac3e52dfd34c09ce486b9c3e33948429185937d6
JeongJu Kim <haje01@gmail.com>
2017-06-30 05:46:43 +0000

    detailed option

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 pyinstaller swak/win_svc.py --hidden-import=win32timezone --onefile
 rem run service test only
 set SWAK_BUILD=TRUE
-coverage run --source swak -a -m pytest tests -k test_svc
+coverage run --source swak --parallel-mode --module pytest tests -k test_svc
