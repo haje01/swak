308495bc6824c2f81a8b49932f6d4f5ab5c8e4b1
JeongJu Kim <haje01@gmail.com>
2017-07-20 19:27:27 +0900

    test

--- swak/cli.py
+++ swak/cli.py
@@ -80,5 +80,7 @@ def prepare_cli(ctx):
     if not getattr(sys, 'frozen', False):
         check_plugins_initpy(enumerate_plugins())
 
+    sys.path.insert(0, os.path.join(get_exe_dir(), 'swak') )
+    logging.debug(sys.path)
     import swak.plugins
     return swak.plugins
